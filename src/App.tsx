11196890561c76a01024b9d8905d1a50755aa6c7
eduvilla97 <eduvilla99@gmail.com>
2025-05-20 19:25:08 +0200

    feat: v0 saving a link and environment config

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,43 @@
 import { useState } from "react";
 
-import reactLogo from "./assets/react.svg";
-
-import viteLogo from "/vite.svg";
 import "./App.css";
-
+import { useConfig } from "./hooks/useConfig";
 function App() {
-  const [count, setCount] = useState(0);
+  const { apiUrl } = useConfig();
+  const [url, setUrl] = useState("");
+  const [shortedUrl, setShortedUrl] = useState("");
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className="flex h-screen flex-col items-center bg-amber-50">
+      <h1>AppCortador de URLs</h1>
+      <input
+        type="text"
+        placeholder="URL"
+        value={url}
+        onChange={(e) => setUrl(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="shortedURL"
+        value={shortedUrl}
+        onChange={(e) => setShortedUrl(e.target.value)}
+      />
+      <button
+        onClick={async () => {
+          const response = await fetch(`${apiUrl}/links`, {
+            method: "POST",
+            body: JSON.stringify({ url, shortedUrl }),
+            headers: {
+              "Content-Type": "application/json"
+            }
+          });
+          const data = await response.text();
+          console.log(data);
+        }}
+      >
+        Cortar
+      </button>
+    </main>
   );
 }
 
---
+++ src/hooks/useConfig.ts
@@ -0,0 +1,5 @@
+export const useConfig = () => {
+  return {
+    apiUrl: import.meta.env.VITE_API_URL
+  };
+};
