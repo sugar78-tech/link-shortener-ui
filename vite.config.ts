1df7450c3b01a48823b6fb400890fc39226b6194
eduvilla97 <eduvilla99@gmail.com>
2025-05-26 21:07:19 +0200

    feat: actualizar título de la aplicación, agregar soporte para SVG y reset de estilos globales

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,26 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import { fileURLToPath } from "url";
+import svgr from "vite-plugin-svgr";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    svgr({
+      svgrOptions: {
+        exportType: "default",
+        ref: true,
+        svgo: false,
+        titleProp: true
+      },
+      include: "**/*.svg"
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
